8b7bf670f18c1446a182ff34a2c0f088fab78088
Naveen kumar boddu <naveenboddu1900@gmail.com>
2026-06-01 09:03:52 +0530

    Export foundation models

---
+++ server/src/models/index.js
@@ -0,0 +1,5 @@
+export { User } from "./User.js";
+export { Workspace } from "./Workspace.js";
+export { Board } from "./Board.js";
+export { List } from "./List.js";
+export { Card } from "./Card.js";
